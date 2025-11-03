88f526fe7962b05823208112fce8276a86397a01
Spyke <vidal.quentin@pog-group.com>
2025-11-04 00:16:53 +0100

    finished v1

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+
+const appRoot = path.join(__dirname, '../public');
+
+
+
+app.get('/', (_req: Request, res: Response) => {
+  res.sendFile('champions.html', { root: appRoot });
+});
+
+
+/* /assets/img/my-image.png
+/assets/blabla.png
+/assets/script/js/javascript/script.js
+/assets/data/logo.svg
+*/
+app.get('/assets/*path', (req: Request, res: Response) => {
+  res.sendFile(req.path, { root: appRoot });
+});
+
+const port = 80;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
